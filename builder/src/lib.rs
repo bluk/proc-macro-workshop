78c7ed74d3e03d64f8103f78a434c459e492c276
Bryant Luk <bryant.luk@bryantluk.com>
2021-02-01 16:55:04 -0600

    Finish Derive Builder Test 1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,12 @@
 use proc_macro::TokenStream;
 
+use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Parse the macro input as a syn::DeriveInput syntax tree.
+    let _input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    // Return an empty TokenStream.
+    TokenStream::new()
 }
