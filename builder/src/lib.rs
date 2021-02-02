6dd6fa9bdc17ab8a3a6a187ca1b32585875be692
Bryant Luk <bryant.luk@bryantluk.com>
2021-02-01 20:48:07 -0600

    Finish Derive Builder Test 6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident};
+use syn::{
+    parse_macro_input, Data, DeriveInput, Fields, GenericArgument, Ident, PathArguments, Type,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -74,8 +76,15 @@ fn build_builder_fields_init(data: &Data) -> TokenStream {
             Fields::Named(ref fields) => {
                 let fields = fields.named.iter().map(|f| {
                     let name = &f.ident;
-                    quote! {
-                        #name: None
+                    let ty = &f.ty;
+                    if find_option_generic_type(ty).is_some() {
+                        quote! {
+                            #name: Some(None)
+                        }
+                    } else {
+                        quote! {
+                            #name: None
+                        }
                     }
                 });
                 quote! {
@@ -96,10 +105,19 @@ fn build_builder_methods_definition(data: &Data) -> TokenStream {
                 let methods = fields.named.iter().map(|f| {
                     let name = &f.ident;
                     let ty = &f.ty;
-                    quote! {
-                        fn #name(&mut self, #name: #ty) -> &mut Self {
-                            self.#name = Some(#name);
-                            self
+                    if let Some(option_generic_ty) = find_option_generic_type(ty) {
+                        quote! {
+                            fn #name(&mut self, #name: #option_generic_ty) -> &mut Self {
+                                self.#name = Some(Some(#name));
+                                self
+                            }
+                        }
+                    } else {
+                        quote! {
+                            fn #name(&mut self, #name: #ty) -> &mut Self {
+                                self.#name = Some(#name);
+                                self
+                            }
                         }
                     }
                 });
@@ -139,5 +157,27 @@ fn build_builder_build_method(name: &Ident, data: &Data) -> TokenStream {
             Fields::Unit | Fields::Unnamed(_) => unimplemented!(),
         },
         _ => unimplemented!(),
+    }
+}
+
+/// Find the generic type if the `ty` argument is an `Option` type.
+fn find_option_generic_type(ty: &Type) -> Option<Type> {
+    match ty {
+        Type::Path(ref ty_path) => {
+            let segments = &ty_path.path.segments;
+            if segments.len() == 1 {
+                if let Some(segment) = segments.first() {
+                    if segment.ident.to_string() == "Option" {
+                        if let PathArguments::AngleBracketed(ref args) = segment.arguments {
+                            if let Some(GenericArgument::Type(generic_ty)) = args.args.first() {
+                                return Some(generic_ty.clone());
+                            }
+                        }
+                    }
+                }
+            }
+            None
+        }
+        _ => None,
     }
 }
