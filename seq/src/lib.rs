76c2070a29c40e15da341ff9da06d653a986eeeb
Bryant Luk <bryant.luk@bryantluk.com>
2021-02-22 09:07:53 -0600

    Finish Seq Test 1

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,36 @@
-use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse::{Parse, ParseStream, Result},
+    parse_macro_input, Expr, Ident, LitInt, Token,
+};
+
+struct Seq {
+    _ident: Ident,
+    _start: LitInt,
+    _end: LitInt,
+    _expr: Expr,
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let ident: Ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let start = input.parse::<LitInt>()?;
+        input.parse::<Token![..]>()?;
+        let end = input.parse::<LitInt>()?;
+        let expr: Expr = input.parse()?;
+        Ok(Seq {
+            _ident: ident,
+            _start: start,
+            _end: end,
+            _expr: expr,
+        })
+    }
+}
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let Seq { .. } = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    proc_macro::TokenStream::from(quote! {})
 }
