21561744c22ac1100d5875132a5d07f63eaf9ba1
Bryant Luk <bryant.luk@bryantluk.com>
2021-02-22 15:36:31 -0600

    Finish Seq Test 8

--- main.rs
+++ main.rs
@@ -8,22 +8,8 @@
 
 use seq::seq;
 
-seq!(N in 16..=20 {
-    enum E {
-        #(
-            Variant#N,
-        )*
+seq!(N in 0..1 {
+    fn main() {
+        let _ = Missing#N;
     }
 });
-
-fn main() {
-    let e = E::Variant16;
-
-    let desc = match e {
-        E::Variant16 => "min",
-        E::Variant17 | E::Variant18 | E::Variant19 => "in between",
-        E::Variant20 => "max",
-    };
-
-    assert_eq!(desc, "min");
-}
